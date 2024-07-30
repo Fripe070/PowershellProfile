0b8109eaec632730a223aeb8038eddb19f13b897
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-07-30 21:43:43 +0200

    Revamp PS1

--- prompt.ps1
+++ prompt.ps1
@@ -1,9 +1,12 @@
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 function Prompt {
+    $LastWasSuccessCopy = $?
+    $LastExitCodeCopy = $LastExitCode
+
     $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
     $principal = [Security.Principal.WindowsPrincipal] $identity
     $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
-    
+
     if (Test-Path variable:/PSDebugContext) { # Check if debug prompt
         Write-Host ('[🛠️]') -NoNewLine -ForegroundColor Grey
     }
@@ -11,13 +14,13 @@ function Prompt {
         Write-Host ('[⚡]') -NoNewLine -ForegroundColor Yellow
     }
 
-    Write-Host (
-        [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split("\")[1]
-        # '@' + 
-        # $([System.Net.Dns]::GetHostName())
-    ) -NoNewLine -ForegroundColor Cyan
+    # Write-Host (
+    #     [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split("\")[1]
+    #     # '@' + 
+    #     # $([System.Net.Dns]::GetHostName())
+    # ) -NoNewLine -ForegroundColor Cyan
 
-    Write-Host (':') -NoNewLine
+    # Write-Host (':') -NoNewLine
     Write-Host ($(Get-Location)) -NoNewLine -ForegroundColor Blue
     
     $branchName = $(git rev-parse --abbrev-ref HEAD)
@@ -27,6 +30,7 @@ function Prompt {
     if ($null -ne $env:VIRTUAL_ENV) { 
         Write-Host (" (venv)") -NoNewLine -ForegroundColor Blue
     }
+    Write-Host (" $('$' * ($nestedPromptLevel + 1))") -ForegroundColor (&{If($LastWasSuccessCopy) {"White"} Else {"Red"}}) -NoNewLine
 
-    return " $('$' * ($nestedPromptLevel + 1)) "
+    return " "
 }
